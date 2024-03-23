d3fab1b0f111923e911543113afbac6e1f67c51e
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-23 16:29:31 -0700

    added starter code

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,9 +1,6 @@
-//importing path module
 const path = require('path');
 
-//exporting the function to send a homepage file. 
-module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-};
+module.exports = (app) =>
+  app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  );
--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
-//importing express to run the app 
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//using client/dist folder as static files, which will be built using webpack 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//onvokes a function exported by htmlRoutes
-require('./routes/htmlRoutes.js')(app);
+require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, function () {
-  console.log(`Now listening on port: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
