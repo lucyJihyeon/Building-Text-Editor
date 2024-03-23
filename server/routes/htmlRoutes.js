6981f71048366d2e86f078ad5c6303417fdae142
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-23 16:31:23 -0700

    added starter code comments

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+//exporting the function to handle homepage route 
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
+//inporting express to run the app 
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//serves client/dist folder as static files 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//invokes a function exported by htmlRoutes to handle homepage route. 
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
