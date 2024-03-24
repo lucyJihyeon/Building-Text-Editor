0c55feff20897039a2c1e1a9df6822dc280a4cca
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-23 23:52:26 -0700

    fixed error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,32 +17,31 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("putDb not implemented");
   //open the jate database with version 1
-  const jateDb = await openDB('jate', 1);
+  const jateDb = await openDB("jate", 1);
   //create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction("jate", "readwrite");
   // Open up the desired object store and get all of the data in the database.
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: id,  text: content });
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log("Data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("getDb not implemented");
   //open the jate database with version 1
   const jateDb = await openDB("jate", 1);
   //create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction("jate", "readonly");
   // Open up the desired object store and get all of the data in the database.
   const store = tx.objectStore("jate");
-  const request = store.getAll();
-
+  const request = store.get(1);
   const result = await request;
-  console.log("result.value", result);
-  return result;
+  result
+    ? console.log("data retrieved from the database", result.value)
+    : console.log("data not found in the database");
+  return result?.value;
 };
 
 initdb();
